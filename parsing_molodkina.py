7840914c883978f24fed4675a257bce694b2dba9
Егор Осипов <egorosipov@MacBook-Air-Egor-3.local>
2025-01-16 04:25:52 +0300

    pandas change

--- parsing_molodkina.py
+++ parsing_molodkina.py
@@ -1,36 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 def get_movies_info(url):
     response = requests.get(url)
 
-    
     if response.status_code == 200:
-        
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        
         movie_titles = soup.find_all('span', class_='text text_block text_fixed text_light_large')
         movie_ratings_site = soup.find_all('span', class_='p-rate-flag__text')
         movie_ratings_imdb = soup.find_all('span', class_='p-rate-flag__imdb-text')
-        movie_genres_countries = soup.find_all('a', class_='color_black underline')
+        movie_genres_countries = soup.find_all('div', class_='margin_top_5')
         movies_info = []
 
-     
         for i in range(len(movie_titles)):
             title = movie_titles[i].get_text(strip=True)
             rating_site = movie_ratings_site[i].get_text(strip=True)
             rating_imdb = movie_ratings_imdb[i].get_text(strip=True)
-            genre = movie_genres_countries[i * 2].get_text(strip=True)
-            country = movie_genres_countries[i * 2 + 1].get_text(strip=True)
+            genre_country_year = movie_genres_countries[i].get_text(strip=True)
 
-            
+            # Разделяем строку на жанр, страну и год
+            parts = genre_country_year.split(',')
+            genre = parts[0].strip()
+            country = parts[1].strip()
+            year = parts[2].strip()
+
             movies_info.append({
                 'title': title,
                 'rating_site': rating_site,
                 'rating_imdb': rating_imdb,
                 'genre': genre,
-                'country': country
+                'country': country,
+                'year': year
             })
 
         return movies_info
@@ -43,9 +45,11 @@ def main():
     url = 'https://kino.mail.ru/cinema/top/'
     movies_info = get_movies_info(url)
 
-    
-    for movie in movies_info:
-        print(f"Наименование жопа: {movie['title']}, Рейтинг от сайта: {movie['rating_site']}, Рейтинг от IMDb: {movie['rating_imdb']}, Жанр: {movie['genre']}, Страна: {movie['country']}")
+    # Преобразуем список словарей в DataFrame
+    df = pd.DataFrame(movies_info)
 
-if __name__ == "__main__":
+    # Выводим DataFrame на экран
+    print(df)
+
+if __name__ == "main":
     main()
